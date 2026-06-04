8b3a35c2e6751b2bd7c3e2442a3bd7df564c79f6
Diogo-Serra <di-code@outlook.com>
2026-06-04 20:08:50 +0100

    update

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -4,9 +4,8 @@
 
 int main(int argc, char** argv) {
 
-    (void)argc;
     if (argc == 1)
-        return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 1);
+        return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, argc);
     for (int i = 1; argv[i]; i++)
     {
         for (int j = 0; argv[i][j]; j++)
